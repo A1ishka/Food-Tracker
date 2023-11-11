04e3b1cae5f3ff3596728b5bd1fde237ed7fb5ee
A1ishka <alinamakogon123456@gmail.com>
2023-11-11 22:55:16 +0300

    JWT Authentication and Authorisation

---
+++ src/main/java/com/makogon/foodtracker/auth/AuthenticationService.java
@@ -0,0 +1,116 @@
+package com.makogon.foodtracker.auth;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.makogon.foodtracker.config.JwtService;
+import com.makogon.foodtracker.user.User;
+import com.makogon.foodtracker.user.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository repository;
+    //private final TokenRepository tokenRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                /*.firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())*/
+                .login(request.getLogin())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .build();
+        /*var savedUser =*/ repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        //var refreshToken = jwtService.generateRefreshToken(user);
+        //saveUserToken(savedUser, jwtToken);
+        return AuthenticationResponse.builder()
+                .accessToken(jwtToken)
+                //.refreshToken(refreshToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getLogin(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByLogin(request.getLogin())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        //var refreshToken = jwtService.generateRefreshToken(user);
+        //revokeAllUserTokens(user);
+        //saveUserToken(user, jwtToken);
+        return AuthenticationResponse.builder()
+                .accessToken(jwtToken)
+                //.refreshToken(refreshToken)
+                .build();
+    }
+
+    /*private void saveUserToken(User user, String jwtToken) {
+        var token = Token.builder()
+                .user(user)
+                .token(jwtToken)
+                .tokenType(TokenType.BEARER)
+                .expired(false)
+                .revoked(false)
+                .build();
+        tokenRepository.save(token);
+    }
+
+    private void revokeAllUserTokens(User user) {
+        var validUserTokens = tokenRepository.findAllValidTokenByUser(user.getId());
+        if (validUserTokens.isEmpty())
+            return;
+        validUserTokens.forEach(token -> {
+            token.setExpired(true);
+            token.setRevoked(true);
+        });
+        tokenRepository.saveAll(validUserTokens);
+    }
+
+    public void refreshToken(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws IOException {
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        final String refreshToken;
+        final String userlogin;
+        if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
+            return;
+        }
+        refreshToken = authHeader.substring(7);
+        userlogin = jwtService.extractUsername(refreshToken);
+        if (userlogin != null) {
+            var user = this.repository.findByLogin(userlogin)
+                    .orElseThrow();
+            if (jwtService.isTokenValid(refreshToken, user)) {
+                var accessToken = jwtService.generateToken(user);
+                revokeAllUserTokens(user);
+                saveUserToken(user, accessToken);
+                var authResponse = AuthenticationResponse.builder()
+                        .accessToken(accessToken)
+                        .refreshToken(refreshToken)
+                        .build();
+                new ObjectMapper().writeValue(response.getOutputStream(), authResponse);
+            }
+        }
+    }*/
+}
